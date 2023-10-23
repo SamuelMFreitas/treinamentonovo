bc9703c320cb6f68bfc983459ee4c15987feada8
Samuel Freitas <mattos_samuel@yahoo.com.br>
2023-10-23 14:05:55 -0300

    Criação do Projeto de Tutorial para EFcore 7

---
+++ treinamentoNerd2/EFCoreTutorialsConsole/Program.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Runtime.CompilerServices;
+
+namespace EFCoreTutorialsConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CriarEntidades();
+
+            LisrtarEstadoEntidades();
+            ListarEstadoAddedEntidade();
+            ListarEstadoModificado();
+
+            ListarEstadoDeletedEntidades();
+            ListarEstadoDetachedEntidade();
+
+        }
+
+        private static void ListarEstadoDetachedEntidade()
+        {
+            var disconnectedEntity = new Student() { StudentId = 1, Name = "Bill" };
+
+            using (var context = new SchoolContext())
+            {
+                Console.Write(context.Entry(disconnectedEntity).State);
+            }
+        }
+
+        private static void ListarEstadoDeletedEntidades()
+        {
+            using (var context = new SchoolContext())
+            {
+                var student = context.Students.FirstOrDefault();
+                context.Students.Remove(student);
+
+                DisplayStates(context.ChangeTracker.Entries());
+            }
+        }
+
+        private static void ListarEstadoModificado()
+        {
+            using (var context = new SchoolContext())
+            {
+                var student = context.Students.FirstOrDefault();
+                student.LastName = "Friss";
+
+                DisplayStates(context.ChangeTracker.Entries());
+            }
+        }
+
+        private static void ListarEstadoAddedEntidade()
+        {
+            using (var context = new SchoolContext())
+            {
+                context.Students.Add(new Student() { FirstName = "Bill", LastName = "Gates" });
+
+                DisplayStates(context.ChangeTracker.Entries());
+            }
+        }
+
+        private static void LisrtarEstadoEntidades()
+        {
+            using (var context = new SchoolContext())
+            {
+                // retrieve entity 
+                var student = context.Students.FirstOrDefault();
+                DisplayStates(context.ChangeTracker.Entries());
+            }
+        }
+
+        static void DisplayStates(IEnumerable<EntityEntry> entries)
+        {
+            foreach (var entry in entries)
+            {
+                Console.WriteLine($"Entidade: {entry.Entity.GetType().Name}, " +
+                    $"State: {entry.State.ToString()}");
+            }
+
+         }
+
+            private static void CriarEntidades()
+        {
+            using (SchoolContext context = new SchoolContext())
+            {
+                //Criar BD
+                context.Database.EnsureCreated();
+
+                //Criar uma entidade de objeto
+                var grade01 = new Grade() { GradeName = "Curso c# .Net" };
+                var student01 = new Student() { FirstName = "Pinóquio", LastName = "Silveirinha", Grade = grade01 };
+
+                //Adicionar a entidade ao contexto
+                context.Students.Add(student01);
+
+                //Salvar a entidade estudante
+                context.SaveChanges();
+
+                foreach (var std in context.Students)
+                {
+                    Console.WriteLine(std.FirstName + " " + std.LastName);
+                }
+            }
+        }
+    }
+}
---
+++ treinamentoNerd2/EFCoreTutorialsConsole/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorialsConsole
+{
+    public class Student
+    {
+       //Sempre que se usa no nome da propriedade o final Id, o Ef entende que deverá
+       //usar essa propriedade como chave primária
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        #region Foreing Key do BD
+        public Grade Grade { get; set; }
+
+        public int GradeId { get; set; }
+
+        #endregion
+
+
+
+
+    }
+}
